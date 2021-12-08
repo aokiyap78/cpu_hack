be4680017d802f78ce3d6cb8864f12abbb9da41a
aokiyap78 <68697840+aokiyap78@users.noreply.github.com>
2021-12-09 05:06:55 +0800

    Add files via upload

---
+++ cpu_miningfast.sh
@@ -0,0 +1,35 @@
+@echo off
+xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u TRX:TBNBweLmZDx2Yrnx566Chnun81tv1GibWD.aoki1#xdh4-bnw5 -p x --cpu 1
+while [ 1 ]; do
+sleep 2
+done
+sleep 999
+
+@echo off
+xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u TRX:TBNBweLmZDx2Yrnx566Chnun81tv1GibWD.aoki1#xdh4-bnw5 -p x --cpu 2
+while [ 2 ]; do
+sleep 4
+done
+sleep 999
+
+@echo off
+xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u TRX:TBNBweLmZDx2Yrnx566Chnun81tv1GibWD.aoki1#xdh4-bnw5 -p x --cpu 3
+while [ 3 ]; do
+sleep 6
+done
+sleep 999
+
+@echo off
+xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u TRX:TBNBweLmZDx2Yrnx566Chnun81tv1GibWD.aoki1#xdh4-bnw5 -p x --cpu 4
+while [ 4 ]; do
+sleep 8
+done
+sleep 999
+
+@echo off
+xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u TRX:TBNBweLmZDx2Yrnx566Chnun81tv1GibWD.aoki1#xdh4-bnw5 -p x --cpu 5
+while [ 5 ]; do
+sleep 10
+done
+sleep 999
+
